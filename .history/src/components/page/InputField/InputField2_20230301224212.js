3406fc8ade6131479e16567a1b1e8cc493e0abbc
vu <iphoneboy46@gmail.com>
2023-03-02 02:48:55 +0700

    update validate

---
+++ .history/src/components/page/InputField/InputField2_20230301224212.js
@@ -0,0 +1,32 @@
+import React ,{useContext} from "react";
+
+function InputField2(props) {
+  const { field, form, type, label, placeholder, disabled } = props;
+  const {account} =  useContext(Context)
+
+  const { name, value, onChange, onBlur } = field;
+  const {errors,touched} = form;
+  const showError = errors[name] && touched[name]
+  return (
+    <div className="flex flex-col mb-5">
+      {label && (
+        <label className="text-[#0F80B3] mb-2 font-bold" htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <input
+        className="email w-[100%]  border-[#CED4DA] border-[1px] px-2 py-2 rounded"
+        type={type}
+        onBlur={onBlur}
+        onChange={onChange}
+        placeholder={placeholder}
+        name={name}
+        id={name}
+        value={account}
+      />
+    {showError && <p className="text-[red] font-bold text-sm">{errors[name]}</p>}
+    </div>
+  );
+}
+
+export default InputField2;
